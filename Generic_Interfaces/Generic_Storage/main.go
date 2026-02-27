000473e2a6f9440339bf610c0f5b718de8e04e77
Valentino <valentinokukurichkaapple@gmail.com>
2026-02-27 18:02:27 +0100

    I added the 'Clear()' metod in the 'Genetic_Storage'

--- Generic_Interfaces/Generic_Storage/main.go
+++ Generic_Interfaces/Generic_Storage/main.go
@@ -5,6 +5,7 @@ import "fmt"
 type Repository[T any] interface {
 	Save(item T)
 	Get() T
+	Clear()
 }
 type MemoryStorage[T any] struct {
 	data T
@@ -17,6 +18,11 @@ func (ms *MemoryStorage[T]) Save(item T) {
 func (ms *MemoryStorage[T]) Get() T {
 	return ms.data
 }
+func (ms *MemoryStorage[T]) Clear() {
+	var zero T // Создаем переменную типа T (для int это 0, для string - "")
+	ms.data = zero
+	fmt.Println("Хранилище очищено")
+}
 
 func main() {
 	strStorage := &MemoryStorage[string]{}
@@ -28,4 +34,7 @@ func main() {
 func useRepository[T any](repo Repository[T], val T) {
 	repo.Save(val)
 	fmt.Printf("Из хранилища получено: %v\n", repo.Get())
+	repo.Clear() // Проверяем наш новый метод
+	fmt.Printf("После очистки: %v\n", repo.Get())
+	fmt.Println("---")
 }
